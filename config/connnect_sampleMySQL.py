6c08418f53d9c05d1c8867f2cd1151d078eb315f
bypcarpark <byp.carpark@gmail.com>
2022-04-19 14:16:05 +0700

    first commit

---
+++ config/connnect_sampleMySQL.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_db',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'POST': '3306',
+
+    }
+}
